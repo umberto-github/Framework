ebf036727bec3d388b5808675e3c29798611dbed
umberto-github <bachetti.u@gmail.com>
2023-11-13 12:04:11 +0100

    resi dinamici tutti i testi dei modali tramite config.json, cambiato colore al footer

--- src/app/services/jsonconfig.service.ts
+++ src/app/services/jsonconfig.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { FrameWorkInfo } from '../structures/frameworkinfo';
+import { Programmers } from '../structures/programmers';
 
 @Injectable({
   providedIn: 'root'
@@ -12,16 +12,20 @@ import { FrameWorkInfo } from '../structures/frameworkinfo';
 export class JsonconfigService {
 
   public jsondata: any;
-  public framewinfo!: FrameWorkInfo;
   public baseUrl = ''; // Sostituisci con l'URL effettivo della tua Web API
   public alarmsUrl = '';
   public anomaliesUrl = '';
   public operatorsUrl = '';
   public logincheckUrl: string = '';
   public appversion: string = '';
+  public frameinfo_system = '';
+  public frameinfo_version = '';
+  public frameinfo_wat = '';
+  public programmers!: Programmers;
+  public contactinfo: string = '';
 
   constructor(private http: HttpClient) {
-    this.framewinfo = new FrameWorkInfo();
+    this.programmers = new Programmers();
     this.http.get('../../assets/config.json').subscribe((data) => {
       this.jsondata = data;
       this.baseUrl = this.jsondata.WebApiBaseUrl
@@ -29,17 +33,19 @@ export class JsonconfigService {
       this.anomaliesUrl = this.jsondata.WebApiAnomalies
       this.operatorsUrl = this.jsondata.WebApiOperators
       this.logincheckUrl = this.jsondata.WebApiLogin
-      this.framewinfo.frameinfo_version = this.jsondata.FrameInfo_Version
-      this.framewinfo.frameinfo_system_os = this.jsondata.FrameInfo_System_OS
-      this.framewinfo.frameinfo_system_ram = this.jsondata.FrameInfo_System_Ram
-      this.framewinfo.frameinfo_system_cpu = this.jsondata.FrameInfo_System_Cpu
-      this.framewinfo.frameinfo_wat_1 = this.jsondata.FrameInfo_WAT_1
-      this.framewinfo.frameinfo_wat_2 = this.jsondata.FrameInfo_WAT_2
-      this.framewinfo.frameinfo_wat_3 = this.jsondata.FrameInfo_WAT_3
-      this.framewinfo.frameinfo_wat_4 = this.jsondata.FrameInfo_WAT_4
+      this.frameinfo_version = this.jsondata.FrameInfo_Version
+      this.frameinfo_system = this.jsondata.FrameInfo_System
+      this.frameinfo_wat = this.jsondata.FrameInfo_WAT
+      //aggiungo l'elemento all'array dei programmatori
+      for (let i = 1; i <= this.jsondata.NumberOfProgrammers; i++) {
+        const propertyName = `Programmers_Pr${i}`;
+        this.programmers.description.push(this.jsondata[propertyName])
+      }
+
+      this.contactinfo = this.jsondata.ContactInfo
       this.appversion = this.jsondata.AppVersion
     });
   }
 
-  
+
 }
